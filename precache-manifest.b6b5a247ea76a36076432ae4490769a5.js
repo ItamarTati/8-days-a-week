3800b9cdcbcf0f0be3f72299f7b4349a7b4f4848
Itamar Tati <itamar.tati1994@gmail.com>
2019-02-19 10:03:06 +0000

    Updates

---
+++ precache-manifest.b6b5a247ea76a36076432ae4490769a5.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "09480accf77316ad56ffb85779c9f5e5",
+    "url": "/8-days-a-week/static/media/posture.09480acc.png"
+  },
+  {
+    "revision": "4e510d12c2203c00ba7481ea5e9919eb",
+    "url": "/8-days-a-week/static/media/person.4e510d12.png"
+  },
+  {
+    "revision": "637e82e40ec2128bccd06109ffde49d2",
+    "url": "/8-days-a-week/static/media/hourcode2016_girllllllllll.637e82e4.png"
+  },
+  {
+    "revision": "e74923f4d1ad3dfdf1739ed10fa361e0",
+    "url": "/8-days-a-week/static/media/food.e74923f4.png"
+  },
+  {
+    "revision": "edff7cdd14afdc5d8fa2",
+    "url": "/8-days-a-week/static/js/runtime~main.edff7cdd.js"
+  },
+  {
+    "revision": "23999b2b849b37028f3f",
+    "url": "/8-days-a-week/static/js/main.23999b2b.chunk.js"
+  },
+  {
+    "revision": "7f4b0209745e9dd3741f",
+    "url": "/8-days-a-week/static/js/1.7f4b0209.chunk.js"
+  },
+  {
+    "revision": "23999b2b849b37028f3f",
+    "url": "/8-days-a-week/static/css/main.89447bc6.chunk.css"
+  },
+  {
+    "revision": "7f4b0209745e9dd3741f",
+    "url": "/8-days-a-week/static/css/1.3c7d68e3.chunk.css"
+  },
+  {
+    "revision": "3d74786b7430a2d1ac51719df72e5f8b",
+    "url": "/8-days-a-week/index.html"
+  }
+];
